b8344b22a03687792db588f32504343580cfd420
Javier de Arcos Velilla <fj.dearcos@gmail.com>
2019-11-05 21:08:59 +0100

    Create console application as demonstrator of sales taxes service

--- src/main/java/es/fjdearcos/salestaxes/SalesTaxesApplication.java
+++ src/main/java/es/fjdearcos/salestaxes/SalesTaxesApplication.java
@@ -1,18 +1,49 @@
 package es.fjdearcos.salestaxes;
 
-
 import es.fjdearcos.salestaxes.configuration.SalesTaxesConfiguration;
+import es.fjdearcos.salestaxes.service.PurchaseProcessorService;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class SalesTaxesApplication {
+import java.util.Scanner;
+
+public class SalesTaxesApplication  implements Runnable {
+
+    private final PurchaseProcessorService purchaseProcessorService;
+
+
+    private SalesTaxesApplication(PurchaseProcessorService purchaseProcessorService) {
+        this.purchaseProcessorService = purchaseProcessorService;
+    }
+
+    @Override
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input purchase:");
+        StringBuilder purchaseDescription = new StringBuilder();
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.isEmpty()) {
+                break;
+            }
+            purchaseDescription.append(line).append("\n");
+        }
+        String receiptDescription = purchaseProcessorService.process(purchaseDescription.toString());
+
+        System.out.println("Output receipt:");
+        System.out.println(receiptDescription);
+    }
+
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
         ctx.register(SalesTaxesConfiguration.class);
         ctx.refresh();
 
-        //ctx.getBean(PurchaseProcessorService.class);
+        PurchaseProcessorService purchaseProcessorService = ctx.getBean(PurchaseProcessorService.class);
+        SalesTaxesApplication salesTaxesApplication = new SalesTaxesApplication(purchaseProcessorService);
+        salesTaxesApplication.run();
 
         ctx.close();
     }
